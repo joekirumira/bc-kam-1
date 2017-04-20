4749623375929c7c1d58fce1fe46988651d07b63
Kirumira <joekirumira3@gmail.com>
2017-04-20 14:33:25 +0300

    added file missing_number.py

---
+++ Missing_Number.py
@@ -0,0 +1,15 @@
+def Missing_Number(num):
+
+      	if len(list) == 0:
+            return 0
+
+    	t = [1,2,3]
+    	x = [1,2,3,4]
+        for t in x:
+    	     return 4
+
+        p = [a,b,c]
+        q = [a,b c]
+        for p in q:
+    	     return 0
+    
